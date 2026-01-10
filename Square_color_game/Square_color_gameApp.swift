98018ee706ed0bb2915a4a13ff0e45bb222dc8c5
ShaznaSalman <shaznasalman10@gmail.com>
2026-01-10 11:34:22 +0530

    Initial Commit

---
+++ Square_color_game/Square_color_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Square_color_gameApp.swift
+//  Square_color_game
+//
+//  Created by COBSCCOMP24.2P-019 on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct Square_color_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
